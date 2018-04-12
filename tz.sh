7f3ba79dda9a7208469ef33fd32dfc7dad5b4b8d
Chmouel Boudjnah <chmouel@chmouel.com>
2018-04-12 12:26:38 +0200

    Add better help

--- tz.sh
+++ tz.sh
@@ -1,32 +1,45 @@
 #!/usr/bin/env bash
-# you can do things like this :
-# % tz
-# % tz 10h30
-# % tz 10h30 next week
-# % tz 11:00 next thursday
-#
-# You can as well add multiple timezones directly on the command line like this :
-#
-# tz +America/Chicago +UTC 10h00 tomorrow
-#
-# By default this script will try to detect your current timezone, if you want
-# to say something like this, show me the different times tomorrow at 10h00 UTC
-# you can do :
-#
-# tz +UTC -t UTC 10h00 tomorrow
-#
-# The order here is important first have the + to add the UTC timezone and set
-# the base timezone to UTC to calculate the others.
-#
-# and so on,
-#
-# This needs gnu date, on MacOSX just install gnuutils from brew
-#
-# This needs bash v4 too, you need to install it from brew as well
-# on MacOSX
-#
+# License: GPL
+# Author: Chmouel Boudjnah <chmouel@chmouel.com>
 set -eo pipefail
 declare -A tzone
+
+function help() {
+    cat <<EOF
+tz allow to calculate different timezone, it allows you to do somethinge like this :
+% tz
+% tz 10h30
+% tz 10h30 next week
+% tz 11:00 next thursday
+
+It will show all different timezone for the timeformat
+
+You can as well add multiple timezones directly on the command line like this :
+
+% tz +America/Chicago +UTC 10h00 tomorrow
+
+By default this script will try to detect your current timezone, if you want
+to say something like this, show me the different times tomorrow at 10h00 UTC
+you can do :
+
+% tz +UTC -t UTC 10h00 tomorrow
+
+The order here is important first have the + to add the UTC timezone and set
+the base timezone to UTC to calculate the others.
+
+and so on,
+
+This needs gnu date, on MacOSX just install gnuutils from brew
+This needs bash v4 too, you need to install it from brew as well
+on MacOSX
+
+EOF
+}
+
+if [[ $1 == "-h" || $1 == "--help" ]];then
+    help
+    exit 0
+fi
 
 ## Change this
 tzone=(
